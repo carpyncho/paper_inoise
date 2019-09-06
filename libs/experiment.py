5f9fe4234ffa024b4a6a9b2ea69cb238589f5ce3
JuanBC <jbc.develop@gmail.com>
2019-09-06 18:13:56 -0300

    cosas

--- libs/experiment.py
+++ libs/experiment.py
@@ -248,9 +248,9 @@ def roc(results, cmap="plasma", ax=None, save_to=None):
     ax.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
     ax.set_xlim([0.0, 1.0])
     ax.set_ylim([0.0, 1.05])
-    ax.set_xlabel('Tasa de Falsos Positivos')
-    ax.set_ylabel('Tasa de Verdaderos Positivos')
-    ax.set_title('Curva ROC')
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('ROC Curve')
     ax.legend(loc="lower right")
     
     plt.tight_layout()
